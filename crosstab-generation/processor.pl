1b6b2cfae09477de0b1bc0444d8da62d60a53b00
Giuseppe Maxia <g.maxia@gmail.com>
2015-08-30 22:28:12 +0200

    improved usability

--- crosstab-generation/processor.pl
+++ crosstab-generation/processor.pl
@@ -1,4 +1,43 @@
-use DBIx::SQLCrosstab;
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use DBIx::SQLCrosstab;    
+use Getopt::Long;
+use Data::Dumper;
+
+my $port     = "5709";
+my $host     = '127.0.0.1';
+my $username = 'msandbox';
+my $password = 'msandbox';
+my $get_help = 0;
+my $result = GetOptions (
+    "port=i"        => \$port,    
+    "host=s"        => \$host,      
+    "user=s"        => \$username,
+    "password=s"    => \$password,
+    "h|help"        => \$get_help,
+    );  
+
+get_help() if $get_help;
+get_help("no params file") unless @ARGV;
+
+sub get_help
+{
+    my ($msg) = @_;
+    print "## $msg\n " if $msg;
+    print <<END_HELP;
+Syntax: $0 [options] params_file
+ --port     = N  (database port : default $port)
+ --host     = N  (database host : default $host)
+ --user     = S  (user name     : default $username)
+ --password = S  (password      : default $password)
+ --help | -h     (This help)
+
+END_HELP
+
+    exit;
+}
 
 sub slurp
 {
@@ -11,14 +50,14 @@ sub slurp
     return join(' ', @rows);
 }
 
-    my $dbh=DBI->connect("dbi:mysql:crosstab;host=127.0.0.1;port=5624",
-        "msandbox","msandbox", {RaiseError=>1})
-            or die "error in connection $DBI::errstr\n";
+my $dbh=DBI->connect("dbi:mysql:crosstab;host=$host;port=$port",
+    $username, $password, {RaiseError=>1})
+        or die "error in connection $DBI::errstr\n";
 
 my $param_file = shift || 'test.pl';
-my $param;
+my $params;
 my $param_text = slurp($param_file);
-eval "\$param=$param_text";
+eval "\$params=$param_text";
 if ($@)
 {
     die "Malformed parameters ($@)\n";
@@ -27,7 +66,8 @@ $params->{dbh} = $dbh;
 
 my $xtab = DBIx::SQLCrosstab->new($params)
     or die "error in creation ($DBIx::SQLCrosstab::errstr)\n";
-
+#print STDERR "#PARAMS\n";
+#print STDERR Dumper $params;
 my $query = $xtab->get_query("_")
     or die "error in query building $DBIx::SQLCrosstab::errstr\n";
 
@@ -35,7 +75,6 @@ if ($ENV{HTML})
 {
     if ($ENV{DEBUG})
     {
-        use Data::Dumper;
         print "<!--\n";
         print Dumper $xtab;
         print "-->\n";
@@ -45,11 +84,12 @@ if ($ENV{HTML})
     my $recs = $xtab->get_recs
         or die "error in execution $DBIx::SQLCrosstab::errstr\n";
 
+    # print Dumper $recs;
     # do something with records, or use the child class 
     # DBIx::SQLCrosstab::Format to produce well 
     # formatted HTML or XML output
-    #
-
+    
+    #    print Dumper $params;
     my $xtab = DBIx::SQLCrosstab::Format->new($params)
         or die "error in creation ($DBIx::SQLCrosstab::errstr)\n";
     if ($xtab->get_query and $xtab->get_recs) 
