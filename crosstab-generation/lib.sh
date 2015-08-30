fd13d20b8717a24d46063e7187eb12aca71cbe6e
Giuseppe Maxia <g.maxia@gmail.com>
2015-08-30 22:30:28 +0200

    Added file to set easily MySQL libraries for systems with only sandboxes

---
+++ crosstab-generation/lib.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=$HOME/opt/mysql/5.7.9/lib:$HOME/opt/mysql/5.7.9/lib/mysql:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$HOME/opt/mysql/5.7.9/lib:$HOME/opt/mysql/5.7.9/lib/mysql:$DYLD_LIBRARY_PATH
+
